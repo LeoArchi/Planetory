c08a690a22144c062b8fbf6eaef3350fd922402a
Léo Perrio <leoperrio@live.fr>
2021-08-25 20:57:41 +0200

    Le test d'ajout de la lune est concluant !

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ require 'astre'
 function love.load()
   sun = Astre:newAstre("Sun", 80, 0)
   earth = Astre:newAstre("Earth", 25, 500, sun)
+  moon = Astre:newAstre("Moon", 5, 50, earth)
   venus = Astre:newAstre("Venus", 23, 300, sun)
 end
 
